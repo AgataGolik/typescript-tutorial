6cb6ac2bc935f67dea7e9b3c29cf17979b459150
Agat Golik <105146311+AgataGolik@users.noreply.github.com>
2024-10-03 13:47:48 +0200

    Update uploadToIpfs.ts

--- scripts/utils/uploadToIpfs.ts
+++ scripts/utils/uploadToIpfs.ts
@@ -1,7 +1,9 @@
+import { IpMetadata } from '@story-protocol/core-sdk'
+
 const pinataSDK = require('@pinata/sdk')
 
-export async function uploadJSONToIPFS(jsonMetadata): Promise<string> {
+export async function uploadJSONToIPFS(ipMetadata: IpMetadata): Promise<string> {
     const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT })
-    const { IpfsHash } = await pinata.pinJSONToIPFS(jsonMetadata)
+    const { IpfsHash } = await pinata.pinJSONToIPFS(ipMetadata)
     return IpfsHash
 }
