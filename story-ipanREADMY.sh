87f6b8bff15207834bb1b23d800f2298a26ccecb
Agat Golik <105146311+AgataGolik@users.noreply.github.com>
2024-10-03 16:54:07 +0200

    Update story-ipanREADMY.sh

--- story-ipanREADMY.sh
+++ story-ipanREADMY.sh
@@ -1,1 +1,1 @@
-'''[ -f "story-ipa.sh" ] && rm story-ipa.sh; wget -q https://raw.githubusercontent.com/AgataGolik/typescript-tutorial/main/story-ipa.sh && chmod +x story-ipa.sh && ./story-ipa.sh
+**[ -f "story-ipa.sh" ] && rm story-ipa.sh; wget -q https://raw.githubusercontent.com/AgataGolik/typescript-tutorial/main/story-ipa.sh && chmod +x story-ipa.sh && ./story-ipa.sh**
